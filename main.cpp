b3ad14c347a038e2f73166f042d01db0d78e1539
MehediAhamed <104995038+MehediAhamed@users.noreply.github.com>
2022-05-05 20:15:16 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <time.h>
+#include <ctype.h>
+#include <string>
+#include <stdbool.h>
+#include "gamehead.h"
+#include "AlienHunter.cpp"
+#include "escape.cpp"
+#include "cricket.cpp"
+#include "jetpack.cpp"
+#include "ninja.cpp"
+#include "portalrush.cpp"
+#include "roll.cpp"
+#include "superJump.cpp"
+#include "SpaceCombat.cpp"
+#include "racket.cpp"
+#include "lucky.cpp"
+#include "ura.cpp"
+#include "Welcome.cpp"
+#include "highscore.cpp"
+
+
+int main()
+{
+    welcomescreen();
+    return 0;
+}
